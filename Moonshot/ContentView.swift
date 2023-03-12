dfa14bda82ebdfb2de93c84dbdb8c5c6d305a298
Brandon Coston <costonb@gmail.com>
2023-03-12 13:47:05 -0700

    Re-initialize
    Reset project back to initial state

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,28 +7,9 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
 
 struct ContentView: View {
-    let layout = [
-        GridItem(.adaptive(minimum: 80, maximum: 120))
-    ]
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(0..<1000) {
-                    Text("Item \($0)")
-                }
-            }
-        }
     }
 }
 
